1ef24de3045bd506f4521931a3c9f82d435cade4
OceanZacharyTam <54297896+OceanZacharyTam@users.noreply.github.com>
2024-02-27 16:24:29 +0800

    script.1.0.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// var map = document.getElementById('map')
-// var one = document.getElementById('one');
-// var two = document.getElementById('two');
-// var three = document.getElementById('three');
-// var four = document.getElementById('four');
-// var five = document.getElementById('five');
-
 var viewfloor = 1;
 var UPfloor = true;
 var ChangeV = false;
@@ -17,7 +10,7 @@ function moveFloor(UPfloor){
     else {
         if (viewfloor <= 1){viewfloor = 7}
         viewfloor --}
-    document.getElementById('one').src = './map/'+viewfloor+'.jpg'}
+    document.getElementById('one').src = './map/XXX.jpg'.replace('XXX',viewfloor)}
 
 
 document.addEventListener('touchstart', (e)=>{
@@ -28,21 +21,9 @@ document.addEventListener('touchend',(e)=>{
     e.preventDefault()
     touchend_x = e.changedTouches[0].clientX
     touchend_y = e.changedTouches[0].clientY
-    // scroll_X = window.scrollX
-    // scroll_Y = window.scrollY
     window_innerWidth = window.innerWidth
     window_innerHeight = window.innerHeight
     finger_no_move = (touchend_x == touchstart_x) && (touchend_y == touchstart_y)
-
-    // document.getElementById('y').innerHTML= 'touchstart_x = ' + touchstart_x
-    //                                         +'<br>touchstart_y = ' + touchstart_y
-    //                                         +'<br>touchend_x = '+touchend_x
-    //                                         +'<br>touchend_y = '+touchend_y
-    //                                         +'<br>scrollX = '+scroll_X
-    //                                         +'<br>scrollY = '+scroll_Y
-    //                                         +'<br>window_innerWidth ='+window_innerWidth
-    //                                         +'<br>finger_no_move = '+(Math.abs(touchstart_x-touchend_x)+Math.abs(touchstart_y-touchend_y))
-
     quadrant_3 = (touchend_x<0.5*window_innerWidth && touchend_y>0.5*window_innerHeight) //為方便單手使用，只有點觸控象限3時才是落樓
     if (finger_no_move && !quadrant_3){moveFloor(true)}
     else if (finger_no_move && quadrant_3){moveFloor(false)}},
